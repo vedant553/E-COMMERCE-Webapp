98c78c973917e60ae7ee57ba47a03a985c40ede3
vedant553 <vedantnandeshwar2003@gmail.com>
2025-08-12 15:54:44 +0530

    CartItems & Cart  Section completed and some modifications in ShopContext and Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState("shop");
+    const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <div className='navbar'>
@@ -26,7 +28,7 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
   )
